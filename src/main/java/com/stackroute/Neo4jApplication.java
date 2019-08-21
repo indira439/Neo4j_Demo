c69c26b82cf52bb2b49bab7101c62ab9799418fa
indu <indira.indukuri@cgi.com>
2019-08-21 10:09:03 +0530

    Created Neo4j spring boot application

--- src/main/java/com/stackroute/Neo4jApplication.java
+++ src/main/java/com/stackroute/Neo4jApplication.java
@@ -3,11 +3,14 @@ package com.stackroute;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Enables Spring Boot auto config and component scanning.
+ */
 @SpringBootApplication
 public class Neo4jApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Neo4jApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jApplication.class, args);
+    }
 
 }
